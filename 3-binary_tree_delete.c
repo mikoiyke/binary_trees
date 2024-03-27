5e19401bd4d60e918f955232bccb8962ece92259
mikoiyke <mikoiyke@gmail.com>
2024-03-26 21:33:50 -0700

    3-binary_tree_delete.c file created

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,13 +7,13 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+	if (tree == NULL)
+		return;
 
-    binary_tree_delete(tree->left);
-	
+	binary_tree_delete(tree->left);
+
 	binary_tree_delete(tree->right);
-    free(tree);
+	free(tree);
 
-    tree = NULL;
+	tree = NULL;
 }
