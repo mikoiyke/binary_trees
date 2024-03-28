8fcb78a683010a2692a2716f06edcd6a7c547751
mikoiyke <mikoiyke@gmail.com>
2024-03-28 03:22:35 -0700

    betty errors fixed for task 9 files)

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,17 +1,23 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of the binary tree, or 0 if tree is NULL
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    return (left_height > right_height ? left_height : right_height) + 1;
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
