43c1b6e90aedd7f074b9e7a933b1413c1ab82c81
mikoiyke <mikoiyke@gmail.com>
2024-04-01 06:21:33 -0700

    .

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,15 +10,15 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 
-    binary_tree_t *grandparent = node->parent->parent;
+	binary_tree_t *grandparent = node->parent->parent;
 
-    if (grandparent->left == node->parent)
-        return (grandparent->right);
-    else if (grandparent->right == node->parent)
-        return (grandparent->left);
-    else
-        return (NULL);
+	if (grandparent->left == node->parent)
+		return (grandparent->right);
+	else if (grandparent->right == node->parent)
+		return (grandparent->left);
+	else
+		return (NULL);
 }
